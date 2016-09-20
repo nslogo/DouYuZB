1766c99e6da3f5eac160be5dadde250951472068
ZoroNie <260420820@qq.com>
2016-09-20 17:17:18 +0800

    首页：自定义滑动的Segment控件，做到点击Title选择与手动滑动页面都能够切换页面，同时Title底部的标识条跟随移动。

--- DouYuZB/DouYuZB/Classes/Home/Controller/HomeViewController.swift
+++ DouYuZB/DouYuZB/Classes/Home/Controller/HomeViewController.swift
@@ -14,14 +14,32 @@ class HomeViewController: UIViewController {
 
     //MARK : - 懒加载属性 
     //pageTitleView {}()闭包
-    fileprivate lazy var pageTitleView : PageTitleView = {
+    fileprivate lazy var pageTitleView : PageTitleView = {[weak self] in
         let titleFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH, width: kScreenW, height: kTitleViewH)
         let titles = ["推荐","游戏","娱乐","趣玩"]
         let titleView = PageTitleView(frame: titleFrame, titles: titles)
-        titleView.backgroundColor = UIColor.blue
+        titleView.delegate = self
         return titleView
     }()
     
+    //pageContentView
+    fileprivate lazy var pageContentView : PageContentView = {[weak self] in
+        
+        let contentH = kScreenH - kStatusBarH - kNavigationBarH - kTitleViewH
+        let contentFrame = CGRect(x: 0, y: kStatusBarH + kNavigationBarH + kTitleViewH, width: kScreenW, height: contentH)
+        
+        //确定所有的子控制器
+        var childVCs = [UIViewController]()
+        for _ in 0..<4 {
+            let vc = UIViewController()
+            vc.view.backgroundColor = UIColor(r: CGFloat(arc4random_uniform(255)), g: CGFloat(arc4random_uniform(255)), b: CGFloat(arc4random_uniform(255)))
+            childVCs.append(vc)
+        }
+        let contentView = PageContentView(frame: contentFrame, childVCs: childVCs, parentViewController: self!)
+        contentView.delegate = self
+        
+        return contentView
+    }()
     
     //MARK : - 系统回调函数
     override func viewDidLoad() {
@@ -39,10 +57,18 @@ class HomeViewController: UIViewController {
 extension HomeViewController {
     
     fileprivate func setupUI() {
+        //不需要调整UIScrollView的内边距，UIScrollView在navigation下默认会加64
+        automaticallyAdjustsScrollViewInsets = false
+        
         //设置导航栏
         setupNavigationBar()
+        
         //添加titleView
         view.addSubview(pageTitleView)
+        
+        //添加contentView
+        view.addSubview(pageContentView)
+        pageContentView.backgroundColor = UIColor.blue
     }
     
     
@@ -65,3 +91,34 @@ extension HomeViewController {
     
     }
 }
+
+
+
+// MARK: - 遵守的PageTitleViewDelegate协议 
+extension HomeViewController : PageTitleViewDelegate {
+    func pageTitleView(titleView: PageTitleView, selectedIndex index: Int) {
+        pageContentView.setCurrentIndex(currentIndex: index)
+    }
+}
+
+// MARK:- 遵守PageContentViewDelegate协议
+extension HomeViewController : PageContentViewDelegate {
+    func pageContentView(contentView: PageContentView, progress: CGFloat, sourceIndex: Int, targetIndex: Int) {
+        pageTitleView.setTitleWithProgress(progress: progress, sourceIndex: sourceIndex, targetIndex: targetIndex)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
